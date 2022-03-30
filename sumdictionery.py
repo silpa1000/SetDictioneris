adc2fda9d938ce8c9500c86d520e44c02f02938d
silpa1000 <100128518+silpa1000@users.noreply.github.com>
2022-03-30 11:53:41 +0530

    Add files via upload

---
+++ sumdictionery.py
@@ -0,0 +1,10 @@
+# sum of values in dictionary
+#initialisation
+
+dic={ 'x':455, 'y':223, 'z':300, 'p':908 }
+
+print("Dictionary: ", dic)
+
+#using sum() and values()
+
+print("sum: ",sum(dic.values()))
